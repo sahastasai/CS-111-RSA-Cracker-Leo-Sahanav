876bd739ab9a406add3711b51f14adceea95d520
sahastasai <sahanavsairamesh@gmail.com>
2026-01-26 12:41:28 -0800

    replaced with long long

--- main.cpp
+++ main.cpp
@@ -7,23 +7,25 @@
 #include <optional>
 using namespace std;
 
-int extendedGCD(int a, int b, int &x, int &y); 
-vector<pair<int, int> > factor(int toFactor);
-pair<int, int>* getPrimeFactorization(int toFactor);
-bool isPrime(int toCheck);
-int modInverse(int a, int m);
-int modPow(int base, int exp, int mod);
-char decodeChar(int num);
-int main()
+long long extendedGCD(long long a, long long b, long long &x, long long &y); 
+vector<pair<long long, long long> > factor(long long toFactor);
+pair<long long, long long>* getPrimeFactorization(long long toFactor);
+bool isPrime(long long toCheck);
+long long modInverse(long long a, long long m);
+long long modPow(long long base, long long exp, long long mod);
+char decodeChar(long long num);
+long long main()
 {
-	int e;
-	int n;
-	int m;
+	try{
+	long long e;
+	long long n;
+	long long m;
 	string line3;
 
 	cin >> e;
 	cin >> n;
 	cin >> m;
+
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	getline(cin, line3);
 
@@ -31,29 +33,29 @@ int main()
 	string token;
 	
 
-	vector<int> message;
+	vector<long long> message;
 	while(ss3 >> token)
 	{
 		message.push_back(stoi(token));
 	}
 	//Variables loaded and set
 	
-	pair<int, int>* pq = getPrimeFactorization(n);
+	pair<long long, long long>* pq = getPrimeFactorization(n);
 	if(pq == nullptr)
 	{
 	
 		cout << "Public key is not valid!" << endl;
 		return 1;
 	}
-	int p = pq->first;
-	int q = pq->second;
+	long long p = pq->first;
+	long long q = pq->second;
 	if(p == q)
 	{
 	
 		cout << "Public key is not valid!" << endl;
 		return 1;
 	}
-	int toMod = (p-1) * (q-1);
+	long long toMod = (p-1) * (q-1);
 	if(e < 1 || e > toMod)
 	{
 		cout << "Public key is not valid!" << endl;
@@ -61,7 +63,7 @@ int main()
 	}
 	//Now, we have everything we need to find d.
 	
-	int d = modInverse(e, toMod);
+	long long d = modInverse(e, toMod);
 	if(d < 0)
 	{
 		cout << "Public key is not valid!" << endl;
@@ -71,23 +73,26 @@ int main()
 	cout << p << " " << q << " " << toMod << " " << d << endl;
 	//Now to decode...
 	vector<char> decodedChars;
-	for(int i = 0; i < m; i++)
+	for(long long i = 0; i < m; i++)
 	{
 		cout << modPow(message[i], d, n) << " ";
 		decodedChars.push_back(decodeChar(modPow(message[i], d, n)));
 	}
 	cout << endl;
-	for(int j = 0; j < m; j++)
+	for(long long j = 0; j < m; j++)
 	{		
 		cout << decodedChars[j];
 	}
 	cout << endl;
-
+	}catch(...)
+	{
+		cerr << "Exception!";
+	}
 	
 
 
 }
-int modPow(int base, int exp, int mod) {
+long long modPow(long long base, long long exp, long long mod) {
 	long long  res = 1;
     long long b = base % mod;
     while (exp > 0) {
@@ -96,9 +101,9 @@ int modPow(int base, int exp, int mod) {
         exp /= 2;
         b = (b * b) % mod;
     }
-    return (int)res;
+    return (long long)res;
 }
-char decodeChar(int num)
+char decodeChar(long long num)
 {
     if(num >= 6 && num <= 26+6)
         return 'A' + (num - 7);
@@ -112,11 +117,11 @@ char decodeChar(int num)
         return '?'; // fallback for unexpected values
 }
 
-vector<pair<int, int> > factor(int toFactor)
+vector<pair<long long, long long> > factor(long long toFactor)
 {
-	int sqr = (int) sqrt(toFactor);
-	vector<pair<int, int> > toReturn;
-	for(int i = 1; i <= sqr; i++)
+	long long sqr = (long long) sqrt(toFactor);
+	vector<pair<long long, long long> > toReturn;
+	for(long long i = 1; i <= sqr; i++)
 	{
 		if(!(toFactor % i))
 		{
@@ -126,40 +131,40 @@ vector<pair<int, int> > factor(int toFactor)
 	return toReturn;
 }
 
-pair<int, int>* getPrimeFactorization(int toFactor)
+pair<long long, long long>* getPrimeFactorization(long long toFactor)
 {
-	vector<pair<int, int> > sairam = factor(toFactor);
-	pair<int, int> current;
+	vector<pair<long long, long long> > sairam = factor(toFactor);
+	pair<long long, long long> current;
 
 	if(sairam.size() == 1)
 	{
-		return new pair<int, int>(sairam[0].first, sairam[0].second);
+		return new pair<long long, long long>(sairam[0].first, sairam[0].second);
 	}
 
-	for(int i = 1; i < sairam.size(); i++)
+	for(long long i = 1; i < sairam.size(); i++)
 	{
 		current = sairam[i];
 		if(isPrime(current.first) && isPrime(current.second)){
-			return new pair<int, int>(current.first, current.second);
+			return new pair<long long, long long>(current.first, current.second);
 		}
 	}
 	return nullptr; 
 }
 
-bool isPrime(int toCheck)
+bool isPrime(long long toCheck)
 {
 	return (factor(toCheck).size() == 1);	
 }
 
-int extendedGCD(int a, int b, int &x, int &y) {
+long long extendedGCD(long long a, long long b, long long &x, long long &y) {
     if (b == 0) {
         x = 1;
         y = 0;
         return a;
     }
 
-    int x1, y1;
-    int gcd = extendedGCD(b, a % b, x1, y1);
+    long long x1, y1;
+    long long gcd = extendedGCD(b, a % b, x1, y1);
 
     x = y1;
     y = x1 - (a / b) * y1;
@@ -167,9 +172,9 @@ int extendedGCD(int a, int b, int &x, int &y) {
     return gcd;
 }
 
-int modInverse(int a, int m) {
-    int x, y;
-    int g = extendedGCD(a, m, x, y);
+long long modInverse(long long a, long long m) {
+    long long x, y;
+    long long g = extendedGCD(a, m, x, y);
 
     if (g != 1) return -1;  
     return (x % m + m) % m;
